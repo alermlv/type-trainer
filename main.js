c5b03bc13ce87ef40257f4b18955f1f742f16e08
alermlv <102925147+alermlv@users.noreply.github.com>
2026-03-01 23:32:07 +0300

    feat: implement CapsLock and Shift state handling
    update keyboard letters based on CapsLock and Shift

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const keys = document.querySelectorAll(".key");
+let isCapsLock = false;
+let isShiftPressed = false;
+
+/* ==========
+  Keyboard Highlight
+========== */
+
 window.addEventListener("keydown", (event) => {
   const key = document.querySelector(`.key[data-code=${event.code}]`);
 
@@ -19,3 +27,40 @@ window.addEventListener("blur", () => {
     key.classList.remove("active")
   );
 });
+
+/* ==========
+  CapsLock & Shift
+========== */
+
+window.addEventListener("keydown", (event) => {
+  isCapsLock = event.getModifierState("CapsLock");
+  
+  if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+    isShiftPressed = true;
+  }
+  
+  updateKeyboardLetters();
+});
+
+window.addEventListener("keyup", (event) => {
+  isCapsLock = event.getModifierState("CapsLock");
+
+  if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+    isShiftPressed = false;
+  }
+
+  updateKeyboardLetters();
+});
+
+function updateKeyboardLetters() {
+  keys.forEach((key) => {
+    const code = key.dataset.code;
+
+    if (!code.startsWith("Key")) return;
+
+    const letter = code.replace("Key", "");
+    const isUpper = isCapsLock ^ isShiftPressed;
+
+    key.textContent = isUpper ? letter.toUpperCase() : letter.toLowerCase();
+  });
+}
